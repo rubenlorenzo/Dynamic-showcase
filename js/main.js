b7dac35a3e6d07140f1af21c6214842c41db5eef
ruben <rubenlorenzo85@yahoo.es>
2020-10-08 10:11:45 +0200

    product and basket, draggable

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function allowDrop(event) {
+    event.preventDefault();
+}
+  
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+
+
+function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    let producto = document.getElementById(data).cloneNode(true);
+    event.target.appendChild(producto);
+}
