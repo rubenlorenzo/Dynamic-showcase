e2af737c76ba65b754cfe9da4e8a501d328788b1
ruben <rubenlorenzo85@yahoo.es>
2020-10-08 12:44:23 +0200

    styling of basket, for products dragables

--- js/main.js
+++ js/main.js
@@ -6,10 +6,17 @@ function drag(event) {
     event.dataTransfer.setData("text", event.target.id);
 }
 
-
+let countProducts=0;
 function drop(event) {
     event.preventDefault();
     var data = event.dataTransfer.getData("text");
     let producto = document.getElementById(data).cloneNode(true);
-    event.target.appendChild(producto);
+    
+    countProducts++;
+    if(countProducts>0){
+        document.getElementById("info").style.display="none";
+    }
+
+    let basket = document.getElementById("basket");
+    basket.appendChild(producto);    
 }
