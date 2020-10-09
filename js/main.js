e86b4e1b2f0d299cdf2d7f8d3b87c7c3a52eecaf
ruben <rubenlorenzo85@yahoo.es>
2020-10-09 17:26:11 +0200

    calc total price

--- js/main.js
+++ js/main.js
@@ -7,14 +7,22 @@ function drag(event) {
 }
 
 let countProducts=0;
+let totalPrice=0;
 function drop(event) {
     event.preventDefault();
+
     var data = event.dataTransfer.getData("text");
     let producto = document.getElementById(data).cloneNode(true);
     
+    let price = document.querySelector("#"+data+" > div").innerText;
+    let priceNumber = Number(price.split("€")[0]);
+    totalPrice= totalPrice+priceNumber;
+    document.getElementById("calc").innerText = totalPrice.toFixed(2);
+
     countProducts++;
     if(countProducts>0){
         document.getElementById("info").style.display="none";
+        document.getElementById("coin").style.display="inline";
     }
 
     let basket = document.getElementById("basket");
