9072881645e58dc8fdd2861e2d8fc7460f6c56ee
ruben <rubenlorenzo85@yahoo.es>
2020-10-16 17:32:46 +0200

    count product repeat

--- js/main.js
+++ js/main.js
@@ -8,11 +8,12 @@ function drag(event) {
 
 let countProducts=0;
 let totalPrice=0;
+let productRepeat = [];
 function drop(event) {
     event.preventDefault();
 
     var data = event.dataTransfer.getData("text");
-    let producto = document.getElementById(data).cloneNode(true);
+    let product = document.getElementById(data).cloneNode(true);
     
     let price = document.querySelector("#"+data+" > div").innerText;
     let priceNumber = Number(price.split("€")[0]);
@@ -24,7 +25,32 @@ function drop(event) {
         document.getElementById("info").style.display="none";
         document.getElementById("coin").style.display="inline";
     }
-
+    
+    //Product Repeat
     let basket = document.getElementById("basket");
-    basket.appendChild(producto);    
+    basket.appendChild(product);
+
+    let productRepeatNode = document.createElement("span");
+    productRepeatNode.className = "product_repeat";
+    product.insertBefore(productRepeatNode, product.children[1]);
+    productRepeat.push(1);    
+    
+    let nodesInBasket = basket.childNodes;    
+    let productsInBasket = [];
+
+    for(let i=0; i<nodesInBasket.length; i++){
+        if(nodesInBasket[i].className == "product"){
+            productsInBasket.push(nodesInBasket[i]);
+        }
+    }
+        
+    for(let i=0; i<(productsInBasket.length-1); i++){       
+        if(productsInBasket[i].id == product.id){            
+            product.style.display = "none";            
+            
+            productRepeat[i]++;           
+            let nodesInArticle = productsInBasket[i].childNodes;            
+            nodesInArticle[3].innerText = productRepeat[i]+" x";   
+        }
+    }
 }
