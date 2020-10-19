409421012a96fc38cf7592adb27270628c6afd43
ruben <rubenlorenzo85@yahoo.es>
2020-10-19 17:47:22 +0200

    collapsed basket

--- js/main.js
+++ js/main.js
@@ -34,23 +34,63 @@ function drop(event) {
     productRepeatNode.className = "product_repeat";
     product.insertBefore(productRepeatNode, product.children[1]);
     productRepeat.push(1);    
-    
-    let nodesInBasket = basket.childNodes;    
-    let productsInBasket = [];
-
-    for(let i=0; i<nodesInBasket.length; i++){
-        if(nodesInBasket[i].className == "product"){
-            productsInBasket.push(nodesInBasket[i]);
-        }
-    }
         
+    let productsInBasket = document.querySelectorAll("#basket .product");
+
     for(let i=0; i<(productsInBasket.length-1); i++){       
         if(productsInBasket[i].id == product.id){            
             product.style.display = "none";            
             
+            product.classList.add("no-show");
             productRepeat[i]++;           
             let nodesInArticle = productsInBasket[i].childNodes;            
             nodesInArticle[3].innerText = productRepeat[i]+" x";   
         }
     }
+
+    //Collapsed basket
+    let productsShow = document.querySelectorAll('#basket .product:not(.no-show)').length
+
+    if(productsShow>6){
+        product.style.display = "none";
+        if(!product.classList.contains("no-show")){
+            if(!document.querySelectorAll('#basket .nohidden')){
+                product.classList.add("hidden");
+            }else{
+                product.classList.add("nohidden");
+            } 
+        }
+        
+    }
+
+    if(productsShow == 6 && !document.querySelector('#button_show_all')){
+        let buttonShowAll = document.createElement("span");
+
+        buttonShowAll.id="button_show_all";
+        buttonShowAll.innerHTML="...";
+        buttonShowAll.addEventListener("click", showAllProductsInBasket);
+
+        basket.appendChild(buttonShowAll);
+    }
+
+    if(document.querySelectorAll('#basket .nohidden')){
+        showAllProductsInBasket()
+    }
+}
+
+function showAllProductsInBasket(){
+    
+    if(document.querySelectorAll('#basket .hidden').length>0){
+        document.querySelectorAll('#basket .hidden').forEach(el => {
+            el.style.display="";
+            el.classList.replace("hidden", "nohidden");
+        });
+
+    }else {
+        document.querySelectorAll('#basket .nohidden').forEach(el => {
+            el.style.display="none";
+            el.classList.replace("nohidden", "hidden");
+        });
+    }
+    
 }
