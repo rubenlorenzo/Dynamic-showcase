d96ed22239f54e5b09a5b0cb2602fbbc31493337
ruben <rubenlorenzo85@yahoo.es>
2020-10-20 12:42:19 +0200

    convert in fuctions the part: calc total price, products repeat, collapse basket

--- js/main.js
+++ js/main.js
@@ -7,18 +7,13 @@ function drag(event) {
 }
 
 let countProducts=0;
-let totalPrice=0;
-let productRepeat = [];
 function drop(event) {
     event.preventDefault();
 
     var data = event.dataTransfer.getData("text");
     let product = document.getElementById(data).cloneNode(true);
     
-    let price = document.querySelector("#"+data+" > div").innerText;
-    let priceNumber = Number(price.split("€")[0]);
-    totalPrice= totalPrice+priceNumber;
-    document.getElementById("calc").innerText = totalPrice.toFixed(2);
+    calcTotalPrice(data);
 
     countProducts++;
     if(countProducts>0){
@@ -26,10 +21,24 @@ function drop(event) {
         document.getElementById("coin").style.display="inline";
     }
     
-    //Product Repeat
     let basket = document.getElementById("basket");
     basket.appendChild(product);
 
+    productsRepeat(product);
+
+    collapseBasket(product,basket);  
+}
+
+let totalPrice=0;
+function calcTotalPrice(data){
+    let price = document.querySelector("#"+data+" > div").innerText;
+    let priceNumber = Number(price.split("€")[0]);
+    totalPrice= totalPrice+priceNumber;
+    document.getElementById("calc").innerText = totalPrice.toFixed(2);
+}
+
+let productRepeat = [];
+function productsRepeat(product){
     let productRepeatNode = document.createElement("span");
     productRepeatNode.className = "product_repeat";
     product.insertBefore(productRepeatNode, product.children[1]);
@@ -47,14 +56,15 @@ function drop(event) {
             nodesInArticle[3].innerText = productRepeat[i]+" x";   
         }
     }
+}
 
-    //Collapsed basket
+function collapseBasket(product){
     let productsShow = document.querySelectorAll('#basket .product:not(.no-show)').length
 
     if(productsShow>6){
         product.style.display = "none";
         if(!product.classList.contains("no-show")){
-            if(!document.querySelectorAll('#basket .nohidden')){
+            if(document.querySelectorAll('#basket .hidden').length > 0) {
                 product.classList.add("hidden");
             }else{
                 product.classList.add("nohidden");
@@ -73,13 +83,12 @@ function drop(event) {
         basket.appendChild(buttonShowAll);
     }
 
-    if(document.querySelectorAll('#basket .nohidden')){
+    if(document.querySelectorAll('#basket .nohidden').length>0){
         showAllProductsInBasket()
     }
 }
 
 function showAllProductsInBasket(){
-    
     if(document.querySelectorAll('#basket .hidden').length>0){
         document.querySelectorAll('#basket .hidden').forEach(el => {
             el.style.display="";
